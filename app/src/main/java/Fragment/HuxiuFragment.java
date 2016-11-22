1e6e723e7537a8561827ae01e83439980e4a25ea
ksheng <294851575@qq.com>
2016-11-22 21:39:11 +0800

    觉醒吧 我的白开水资讯~

---
+++ app/src/main/java/Fragment/HuxiuFragment.java
@@ -0,0 +1,159 @@
+package Fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.wuxiaolong.pullloadmorerecyclerview.PullLoadMoreRecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Adapter.HuxiuAdapter;
+import Adapter.RecyclerViewBaseAdapter;
+import DataBean.HuXiuBean;
+import MyUtils.LogUtils;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import cn.bmob.v3.Bmob;
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.FindListener;
+import skkk.cleanwaterinformation.R;
+import skkk.cleanwaterinformation.WebActivity;
+
+/**
+ * Created by admin on 2016/11/21.
+ */
+/*
+* 
+* 描    述：展示虎嗅网的Fragment
+* 作    者：ksheng
+* 时    间：2016/11/21$ 21:16$.
+*/
+public class HuxiuFragment extends Fragment {
+    @Bind(R.id.rv_huxiu)
+    PullLoadMoreRecyclerView rvHuxiu;
+
+    private int page=0;
+    private List<HuXiuBean> mDataList=new ArrayList<HuXiuBean>();
+    private HuxiuAdapter adapter;
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Bmob.initialize(getContext(), "9e16e39fa5374f446e5c928da0f83d62");
+        View huxiuView=inflater.inflate(R.layout.fragment_huxiu,container,false);
+        ButterKnife.bind(this,huxiuView);
+        initUI();
+        initData();
+        initEvent();
+
+        return huxiuView;
+    }
+
+    
+
+    /*
+    ***************************************************
+    * @方法 初始化UI
+    * @参数
+    * @返回值
+    */
+    private void initUI() {
+        /* @描述 设置Adapter */
+        adapter = new HuxiuAdapter(getContext(),mDataList);
+        /* @描述 布局 */
+        rvHuxiu.setLinearLayout();
+        /* @描述 设置间距 */
+        SpacesItemDecoration mDecoration = new SpacesItemDecoration(3);
+        /* @描述 添加间距 */
+        rvHuxiu.addItemDecoration(mDecoration);
+        /* @描述 设置基本动画 */
+        rvHuxiu.setItemAnimator(new DefaultItemAnimator());
+        /* @描述 rvNoteList */
+        rvHuxiu.setAdapter(adapter);
+    }
+
+    /*
+    ***************************************************
+    * @方法 获取数据
+    * @参数
+    * @返回值
+    */
+    private void initData() {
+        BmobQuery<HuXiuBean> query = new BmobQuery<HuXiuBean>();
+        //查询playerName叫“比目”的数据
+        query.setSkip(page*20);
+        //返回50条数据，如果不加上这条语句，默认返回10条数据
+        query.setLimit(20);
+        //执行查询方法
+        query.findObjects(new FindListener<HuXiuBean>() {
+            @Override
+            public void done(List<HuXiuBean> object, BmobException e) {
+                if(e==null){
+                    LogUtils.Log("查询成功：共"+object.size()+"条数据。");
+                    for (int i = 0; i < object.size(); i++) {
+                        mDataList.add(object.get(i));
+                    }
+                    adapter.append(object);
+                    if (rvHuxiu.isLoadMore()) {
+                        rvHuxiu.setPullLoadMoreCompleted();
+                    }
+                }else{
+                    Log.i("bmob","失败："+e.getMessage()+","+e.getErrorCode());
+                }
+            }
+        });
+    }
+
+    
+    /*
+    ***************************************************
+    * @方法 设置监听事件
+    * @参数
+    * @返回值
+    */
+    private void initEvent() {
+        rvHuxiu.setOnPullLoadMoreListener(new PullLoadMoreRecyclerView.PullLoadMoreListener() {
+            @Override
+            public void onRefresh() {
+                Toast.makeText(getContext(), "下拉刷新", Toast.LENGTH_SHORT).show();
+                rvHuxiu.setPullLoadMoreCompleted();
+            }
+
+            @Override
+            public void onLoadMore() {
+                Toast.makeText(getContext(), "上拉加载更多", Toast.LENGTH_SHORT).show();
+                page++;
+                initData();
+
+            }
+        });
+
+        adapter.setOnItemClickLitener(new RecyclerViewBaseAdapter.OnItemClickLitener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Intent itemIntent=new Intent();
+                itemIntent.putExtra("url",mDataList.get(position).getContentURL());
+                itemIntent.putExtra("title",mDataList.get(position).getTitle());
+                itemIntent.setClass(getContext(), WebActivity.class);
+                startActivity(itemIntent);
+            }
+
+            @Override
+            public void onItemLongClick(View view, int position) {
+
+            }
+        });
+    }
+}
